f9cf4dc09f4126025762cec7e93795242104040e
abhixyz1 <abhinayazrf@gmail.com>
2025-10-23 23:08:10 +0700

    add model relationship

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Room extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'floor_id','name','capacity','type','facilities','price_hour','price_day'
+    ];
+
+    protected $casts = [
+        'facilities' => 'array',
+        'price_hour' => 'decimal:2',
+        'price_day'  => 'decimal:2',
+    ];
+
+    public function floor() {
+        return $this->belongsTo(Floor::class);
+    }
+
+    public function bookings() {
+        return $this->hasMany(Booking::class);
+    }
 }
